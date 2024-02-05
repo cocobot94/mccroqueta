cb1acad3c137667f258ea556821451c5d4bfa4ae
Elier <124456513+cocobot94@users.noreply.github.com>
2024-02-05 12:12:59 -0500

    Actualizar production.py

--- mrcroqueto/mrcroqueto/settings/production.py
+++ mrcroqueto/mrcroqueto/settings/production.py
@@ -18,36 +18,7 @@ import dj_database_url
 # Configuración de la base de datos para Heroku Postgres
 DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
 
-"""DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-}
-}"""
-
-"""DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "d7i3340qqkik0p",
-        "PASSWORD": "44303b6cd978c82fd206bd6874309a48abd66cdaf3d52b0acd7e23fd0098a005",
-        "USER": "oxmqtxmmgpafld",
-        "HOST": "ec2-3-232-218-211.compute-1.amazonaws.com",
-        "PORT": "5432",
-    }
-}"""
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "..//apps/core/static"),)
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# django_heroku.settings(locals())
